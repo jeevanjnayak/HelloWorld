e407b8e6835326b0911ece3e9ec7ec70971c63b7
jeevanjnayak <jeevanjnayak@gmail.com>
2022-01-13 15:48:00 +0530

    comparison of 2 words

---
+++ src/com/company/StringComparision.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class StringComparision {
+    public static void main(String[] args){
+        String s1="AnyWord";
+        String s2="AnyWord";
+
+        System.out.println(s1.equals(s2));
+    }
+}
